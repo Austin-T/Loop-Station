f065517dc2231b8294d37baefba032c00654e549
Austin Tralnberg <atralnberg@gmail.com>
2020-05-19 15:42:00 -0600

    style changes

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ for (let i = 0; i < states.length; i++) {
             // save the current notes in the looper
             savedStates[i] = [...notes];
             // change the color of the button
-            states[i].style.backgroundColor = "yellow";
+            states[i].style.backgroundColor = "rgb(179, 164, 37)";
             states[i].innerHTML = "Restore State";
             states[i].style.textAlign = "center";
         }
@@ -210,7 +210,7 @@ let playbtn = document.getElementById("play");
 playbtn.style.backgroundColor = "white";
 playbtn.addEventListener("click", function() {
     if (this.style.backgroundColor == "white") {
-        this.style.backgroundColor = "blue";
+        this.style.backgroundColor = "rgb(37, 86, 179)";
         play = true;
     }
     else {
@@ -220,10 +220,10 @@ playbtn.addEventListener("click", function() {
 });
 
 let metronomebtn = document.getElementById("metronome");
-metronomebtn.style.backgroundColor = "blue";
+metronomebtn.style.backgroundColor = "rgb(37, 86, 179)";
 metronomebtn.addEventListener("click", function() {
     if (this.style.backgroundColor == "white") {
-        this.style.backgroundColor = "blue";
+        this.style.backgroundColor = "rgb(37, 86, 179)";
         createMetronome();
     }
     else {
@@ -238,42 +238,46 @@ clearLoop.addEventListener('click', function(){
     notes = [];
 });
 
-//window.addEventListener('load', () => {
-    const sounds = document.querySelectorAll(".sound");
-    const pads = document.querySelectorAll(".pad");
-    const visual = document.querySelector(".visual");
-    const colors = [
-        "rgb(58, 173, 58)",
-        "rgb(179, 164, 37)",
-        "rgb(160, 72, 32)",
-        "rgb(175, 40, 47)",
-        "rgb(147, 50, 167)",
-        "rgb(37, 86, 179)"
-    ];
-    const volumeRange = document.getElementById("volumeRange");
-    const speedRange = document.getElementById("speedRange");
 
-    pads.forEach((pad, index) => {
-        pad.addEventListener('click', function() {
-                sounds[index].currentTime = 0;
-                sounds[index].volume = volumeRange.value / 100;
-                sounds[index].playbackRate = speedRange.value / 100;
-                sounds[index].play();
-                createWave(colors[Math.floor(Math.random() * 6)]);
-                addNote(sounds[index], colors[Math.floor(Math.random() * 6)]);
-        });
+const sounds = document.querySelectorAll(".sound");
+const pads = document.querySelectorAll(".pad");
+const visual = document.querySelector(".visual");
+const colors = [
+    "rgb(58, 173, 58)",
+    "rgb(179, 164, 37)",
+    "rgb(160, 72, 32)",
+    "rgb(175, 40, 47)",
+    "rgb(147, 50, 167)",
+    "rgb(37, 86, 179)"
+];
+const volumeRange = document.getElementById("volumeRange");
+const speedRange = document.getElementById("speedRange");
+
+pads.forEach((pad, index) => {
+    pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
+            sounds[index].volume = volumeRange.value / 100;
+            sounds[index].playbackRate = speedRange.value / 100;
+            sounds[index].play();
+            createWave(colors[Math.floor(Math.random() * 6)]);
+            addNote(sounds[index], colors[Math.floor(Math.random() * 6)]);
     });
+});
 
-    const createWave = (color) => {
-        for (var i = 0; i < 10; i++){
-            const wave = document.createElement("div");
-            visual.appendChild(wave);
-            wave.style.backgroundColor = color;
-            wave.style.left = String(Math.floor(Math.random()*700)) + "px";
-            wave.style.animation = 'jump 1s ease';
-            wave.addEventListener('animationend', function(){
-                visual.removeChild(this);
-            });
-        }
+const createWave = (color) => {
+    for (var i = 0; i < 10; i++){
+        const wave = document.createElement("div");
+        visual.appendChild(wave);
+        wave.style.backgroundColor = color;
+        wave.style.left = String(Math.floor(Math.random()*700)) + "px";
+        wave.style.animation = 'jump 1s ease';
+        wave.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
     }
-//});
+}
+
+window.addEventListener('load', function() {
+    const preload = document.getElementById("preload");
+    preload.classList.add("preload-finish");
+});
