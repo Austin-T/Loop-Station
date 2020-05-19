8685baecf354df5e65e32747d51d22861e7f9d7c
Austin Tralnberg <atralnberg@gmail.com>
2020-05-19 17:15:50 -0600

    Code Reorganized and Audio Enhanced

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Classes
+
 class Paddle {
     constructor(loopWidth, loopHeight) {
         this.loopWidth = loopWidth;
@@ -23,6 +25,10 @@ class Paddle {
     getXPos(){
         // This function returns the current x position of the paddle
         return this.position.x;
+    }
+    getXPosAfter(){
+        // This function returnt the x position of the leading edge of the paddle
+        return this.position.x + this.width;
     }
     getYPos(){
         // This function returns the current x position of the paddle
@@ -77,6 +83,8 @@ class Note {
         this.justPlayed = false;
     }
 }
+
+// Functions
 
 function playLoop(timestamp) {
     // This function is responisble for plaing the looper and allowing new recording
@@ -148,42 +156,68 @@ function drawObjects() {
 function addNote(sound, color) {
     if (!play) return;
     let sound2 = new Audio(sound.src);
-    let newNote = new Note(sound2, color, paddle.getXPos(), paddle.getYPos());
+    let newNote = new Note(sound2, color, paddle.getXPosAfter(), paddle.getYPos());
     notes.push(newNote);
 }
+
+function createMetronome(beats) {
+    for (let i = 0; i <= beats; i++) {
+        let audio = new Audio('./sounds/click.mp3');
+        audio.volume = 0.2;
+        metronome.push(new Note(audio, "grey", (i + 1) * (300 / beats), 0));
+    }
+}
+
+function createWave(color) {
+    for (var i = 0; i < 10; i++){
+        const wave = document.createElement("div");
+        visual.appendChild(wave);
+        wave.style.backgroundColor = color;
+        wave.style.left = String(Math.floor(Math.random()*700)) + "px";
+        wave.style.animation = 'jump 1s ease';
+        wave.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    }
+}
+
+// Global variables
+
+const canvas = document.getElementById("loopScreen");
+const context = canvas.getContext("2d");
+const sounds = document.getElementsByClassName("sound");
+const pads = document.getElementsByClassName("pad");
+const visual = document.getElementById("visual");
+const colors = [
+    "rgb(58, 173, 58)",
+    "rgb(179, 164, 37)",
+    "rgb(160, 72, 32)",
+    "rgb(175, 40, 47)",
+    "rgb(147, 50, 167)",
+    "rgb(37, 86, 179)"
+];
+const volumeRange = document.getElementById("volumeRange");
+const speedRange = document.getElementById("speedRange");
+const tempoRange = document.getElementById("tempoRange");
+const states = document.getElementsByClassName("state");
+const metronomebtn = document.getElementById("metronome");
+const playbtn = document.getElementById("play");
+const clearLoop = document.getElementById("clearLoop");
+
+playbtn.style.backgroundColor = "white";
+metronomebtn.style.backgroundColor = "rgb(37, 86, 179)";
 
 let play = false;
 let width = 500;
 let height = 150;
-let canvas = document.getElementById("loopScreen");
-console.log(canvas.style.width);
-let context = canvas.getContext("2d");
 let paddle = new Paddle (width, height);
 let notes = [];        // an array to hold all of the Note objects
 let metronome = [];    // an array to hold the position of the metronome clicks
 let lastUpdate = 0;    // holds the time of the last update to game objects
-//let paddleSpeed = 5;
 let savedStates = [[], [], [], [], []]; // holds arrays of notes saved by the user
 
+// Event Listeners
 
-
-// create the metronome
-function createMetronome() {
-    for (let i = 0; i <= 300; i++) {
-        if (i % 15 == 0) {
-            let audio = new Audio('./sounds/click.mp3');
-            audio.volume = 0.2;
-            metronome.push(new Note(audio, "grey", i, 0));
-        }
-    }
-}
-createMetronome();
-
-const tempoRange = document.getElementById("tempoRange");
-
-playLoop(0);
-
-let states = document.getElementsByClassName("state");
 for (let i = 0; i < states.length; i++) {
     states[i].style.backgroundColor = "white";
     states[i].addEventListener("click", function() {
@@ -191,7 +225,7 @@ for (let i = 0; i < states.length; i++) {
             // save the current notes in the looper
             savedStates[i] = [...notes];
             // change the color of the button
-            states[i].style.backgroundColor = "rgb(179, 164, 37)";
+            states[i].style.backgroundColor = "rgb(58, 173, 58)";
             states[i].innerHTML = "Restore State";
             states[i].style.textAlign = "center";
         }
@@ -206,8 +240,6 @@ for (let i = 0; i < states.length; i++) {
     });
 }
 
-let playbtn = document.getElementById("play");
-playbtn.style.backgroundColor = "white";
 playbtn.addEventListener("click", function() {
     if (this.style.backgroundColor == "white") {
         this.style.backgroundColor = "rgb(37, 86, 179)";
@@ -219,12 +251,10 @@ playbtn.addEventListener("click", function() {
     }
 });
 
-let metronomebtn = document.getElementById("metronome");
-metronomebtn.style.backgroundColor = "rgb(37, 86, 179)";
 metronomebtn.addEventListener("click", function() {
     if (this.style.backgroundColor == "white") {
         this.style.backgroundColor = "rgb(37, 86, 179)";
-        createMetronome();
+        createMetronome(8);
     }
     else {
         this.style.backgroundColor = "white";
@@ -232,52 +262,28 @@ metronomebtn.addEventListener("click", function() {
     }
 });
 
-
-let clearLoop = document.getElementById("clearLoop");
 clearLoop.addEventListener('click', function(){
     notes = [];
 });
 
-
-const sounds = document.querySelectorAll(".sound");
-const pads = document.querySelectorAll(".pad");
-const visual = document.querySelector(".visual");
-const colors = [
-    "rgb(58, 173, 58)",
-    "rgb(179, 164, 37)",
-    "rgb(160, 72, 32)",
-    "rgb(175, 40, 47)",
-    "rgb(147, 50, 167)",
-    "rgb(37, 86, 179)"
-];
-const volumeRange = document.getElementById("volumeRange");
-const speedRange = document.getElementById("speedRange");
-
-pads.forEach((pad, index) => {
-    pad.addEventListener('click', function() {
-            sounds[index].currentTime = 0;
-            sounds[index].volume = volumeRange.value / 100;
-            sounds[index].playbackRate = speedRange.value / 100;
-            sounds[index].play();
-            createWave(colors[Math.floor(Math.random() * 6)]);
-            addNote(sounds[index], colors[Math.floor(Math.random() * 6)]);
+for (let i = 0; i < pads.length; i++) {
+    pads[i].style.backgroundColor = colors[Math.floor(i / 4)];
+    pads[i].addEventListener('click', function() {
+            sounds[i].currentTime = 0;
+            sounds[i].volume = volumeRange.value / 100;
+            sounds[i].playbackRate = speedRange.value / 100;
+            sounds[i].play();
+            createWave(pads[i].style.backgroundColor);
+            addNote(sounds[i], pads[i].style.backgroundColor);
     });
-});
-
-const createWave = (color) => {
-    for (var i = 0; i < 10; i++){
-        const wave = document.createElement("div");
-        visual.appendChild(wave);
-        wave.style.backgroundColor = color;
-        wave.style.left = String(Math.floor(Math.random()*700)) + "px";
-        wave.style.animation = 'jump 1s ease';
-        wave.addEventListener('animationend', function(){
-            visual.removeChild(this);
-        });
-    }
 }
 
 window.addEventListener('load', function() {
     const preload = document.getElementById("preload");
     preload.classList.add("preload-finish");
 });
+
+// Function calls
+
+createMetronome(8);
+playLoop(0);
